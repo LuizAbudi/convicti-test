2d81c11030eaf48971c1c303c2430289830ce901
LuizAbudi <luizabudi@hotmail.com>
2025-03-19 17:17:34 -0300

    chore: initial commit

---
+++ src/views/HomeView.vue
@@ -0,0 +1,11 @@
+<script setup lang="ts">
+</script>
+
+<template>
+  <main>
+    <div class="p-2 text-center">
+      <h1 class="text-4xl">Welcome to Vite + Vue 3 + TypeScript + Tailwind CSS</h1>
+      <p class="text-lg">This is the Home view</p>
+    </div>
+  </main>
+</template>
